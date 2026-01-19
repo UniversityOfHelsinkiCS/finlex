3b0c0f58b2e40078f91408e9b95b1612225fa6e6
matiassinisalo <matias.aleksanteri@hotmail.fi>
2026-01-19 16:42:07 +0200

    environment to sentry

--- frontend/src/util/sentry.tsx
+++ frontend/src/util/sentry.tsx
@@ -4,10 +4,12 @@ import * as Sentry from "@sentry/react";
 if you are getting duplicate sentry errors, remember to feed an environment variable to init to make apart different staging or production environments
 */
 export const initSentry = () => {
+  console.log("sentry started")
   Sentry.init({
     dsn: "https://3ceddfdc2ec34770c94e954ead69872b@toska.it.helsinki.fi/23",
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
+    environment: 'production',
     sendDefaultPii: true,
     integrations: [
       Sentry.browserTracingIntegration(),
